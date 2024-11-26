d1b26a22dff9baaa50b347ef7459df0916358c36
varunarvind411 <va411@live.mdx.ac.uk>
2024-11-26 15:03:05 +0400

    Add files via upload

---
+++ products.js
@@ -0,0 +1,47 @@
+let products=[ 
+    {
+    id: 1001,
+    title: "Cat food 25lb bag",
+    description: "A 25 pound bag of <em>irresistable</em>," + " organic goodness for your cat.",
+    price: " AED 2000",
+    image: "images/catfood.png",
+    availableinventory: 10,
+    rating:3
+},
+{
+    id: 1002,
+    title: "Yarn",
+    description: "HIGH QUALITY YARN: Included in our set are 20 yarn skeins. The yarn is made from 100% acrylic and each skein is 25g in weight.",
+    price: " AED 2000",
+    image: "images/yarn.png",
+    availableinventory: 7,
+    rating:2
+},
+{
+    id: 1003,
+    title: "Gaming Chair",
+    description: "Vigosit gaming chair with massager is specially designed for sedentary people who are prone to low back pain. Intelligent finger sensation massage, circular kneading stiff waist for deep relaxation, effectively reducing discomfort.",
+    price: " AED 200",
+    image: "images/gamingchair.png",
+    availableinventory: 7,
+    rating:4
+},
+{
+    id: 1004,
+    title: "BOPai Intelligent expandable large Smart 15.6 inch Backpack",
+    description: " This backpack has smooth zippers on the back side. Once pull on the intelligent zipper, the invisible compartment will show up and equipped with extra 50% of space. Best choice for short journey and easy your daily life.",
+    price: " AED 300",
+    image: "images/backpack.png",
+    availableinventory: 7,
+    rating:4  
+},
+{
+    id: 1005,
+    title: "ODK 63 inch Computer Desk with Power Outlets & LED Lights",
+    description: " This This desk comes with a nice aesthetic look and feel for gaming and office.",
+    price: " AED 300",
+    image: "images/backpack.png",
+    availableinventory: 7,
+    rating:4  
+}
+]
